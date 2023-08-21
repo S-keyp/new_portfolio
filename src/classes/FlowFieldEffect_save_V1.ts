1d400242fdbcf047a6ab91154745c3ea3158f377
Odin <victor.odin@sereema.com>
2023-08-21 10:27:24 +0200

    FlowFieldV1 save + add color

---
+++ src/classes/FlowFieldEffect_save_V1.ts
@@ -0,0 +1,62 @@
+import AbstractCanvas from "./AbstractCanvas"
+
+export default class FlowFieldEffect extends AbstractCanvas {
+    i = 0
+    incr = 1
+    gradient: CanvasGradient
+
+    mouse = {
+        x: this.width / 2,
+        y: this.height / 2
+    }
+
+    constructor( canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, width: number, height: number ) {
+        super(canvas, ctx, width, height)
+        this.ctx.lineWidth = 2
+
+        this.createGradient()
+        this.ctx.strokeStyle = this.gradient
+
+        this.animate()
+
+    }
+
+    createGradient(){
+        this.gradient = this.ctx.createLinearGradient(0, 0, this.width, this.height)
+        this.gradient.addColorStop( .2, '#FF5C33' )
+        this.gradient.addColorStop( .4, '#ccccff' )
+        this.gradient.addColorStop( .6, '#b3ffff' )
+        this.gradient.addColorStop( .8, '#80ff80' )
+        this.gradient.addColorStop( 1, '#FFFF33' )
+    }
+
+    resizeCanvas(width: number, height: number): void{
+        // ajouter 2ème condition pour s'assurer que ça reste un rectangle
+        if(width > 1.25 * height ){
+            this.width = width
+            this.height = height
+        }
+    }
+
+    setMousePosition(x: number, y: number){
+        this.mouse.x = x
+        this.mouse.y = y
+    }
+
+    draw(x: number, y: number){
+        const length = 300
+        this.ctx.beginPath()
+        this.ctx.moveTo(x, y)
+        this.ctx.lineTo(this.mouse.x, this.mouse.y)
+        this.ctx.stroke()
+    }
+    
+    animate(){  
+        // this.ctx.clearRect(0, 0, this.width, this.height)
+        this.draw(this.width / 2, this.height / 2)
+
+        this.animationId = requestAnimationFrame(this.animate.bind(this))
+    }
+
+
+}
