78768e3bee6b63b0f8012f4562ef90a26dfbfafc
S-keyp <vicodin@orange.fr>
2023-09-12 21:16:25 +0200

    Feat: Add dist for gh page deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import replace from '@rollup/plugin-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: '/new_portfolio/',
+
+  plugins: [
+    replace({
+      'process.env.OPENWEATHERMAP_API_KEY': JSON.stringify('7bddfc91ef12d670b8ae3ba9b9c51e45')
+    }),
+    vue()
+  ],
+
+  build: {
+    rollupOptions: {
+
+    }
+  }
 })
