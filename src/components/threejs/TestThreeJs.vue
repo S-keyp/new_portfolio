90dfc2770787a0f4188a0c29ac5822c8580a4b75
S-keyp <vicodin@orange.fr>
2024-01-25 23:40:39 +0100

    test deploy vercel that may break gh page

--- src/components/threejs/TestThreeJs.vue
+++ src/components/threejs/TestThreeJs.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { computed, onMounted } from 'vue';
+    import { onMounted } from 'vue';
     import CanvasThreejs from '../../classes/canvas/threejs/Test';
 
     let canvasThree = null as CanvasThreejs | null;
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import replace from '@rollup/plugin-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/new_portfolio/',
 
   plugins: [
-    replace({
-      'process.env.OPENWEATHERMAP_API_KEY': JSON.stringify('a'),
-      'preventAssignment': true,
-    }),
     vue()
   ],
 
   build: {
     rollupOptions: {
-      
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
     }
-  }
+}
 })
